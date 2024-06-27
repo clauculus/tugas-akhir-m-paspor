cd7613f90192553d3e2d2c006ea55b5bce08069a
Claudia <88203931+clauculus@users.noreply.github.com>
2024-06-28 01:48:58 +0700

    style: hasil pencarian

--- app/lokasi/hasilPencarian.js
+++ app/lokasi/hasilPencarian.js
@@ -35,7 +35,7 @@ export default function HasilPencarian() {
         source={require("../../assets/images/gradient-bg.png")}
         style={styles.navbar}
       >
-        <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <View style={{ flexDirection: "row" }}>
           <Ionicons
             name="chevron-back"
             color={colors.white}
@@ -52,50 +52,189 @@ export default function HasilPencarian() {
         </View>
       </ImageBackground>
 
-      <ScrollView style={{ flex: 1, padding: 16 }}>
-        <Text>Results with Quota</Text>
+      <ScrollView style={{ flex: 1, padding: 24 }}>
+        {/* <Text>Results with Quota</Text> */}
         {lokasi.resultsWithKuota.map((item, index) => (
           <View
             key={index}
             style={{
-              padding: 10,
-              borderBottomWidth: 1,
-              backgroundColor: "pink",
+              backgroundColor: "white",
+              shadowColor: "#000",
+              shadowOffset: { width: 1, height: 1 },
+              shadowOpacity: 0.25,
+              shadowRadius: 6,
+              elevation: 1,
               borderRadius: 18,
+              padding: 23,
+              borderWidth: 0.5,
+              borderColor: "#6FA39A",
+              gap: 2,
+              marginBottom: 10,
             }}
           >
-            <Text>{item.lokasi.nama}</Text>
-            <Text>{item.lokasi.nama}</Text>
-            <Text>Kuota permohonan reguler : {item.totalKuota}</Text>
-            <Text>{item.lokasi.isBiasa ? "biasa" : "ga biasa"}</Text>
-            <Button
-              onPress={() =>
-                router.push({
-                  pathname: "/lokasi/pilihJadwal",
-                  params: {
-                    lokasi: JSON.stringify(item),
-                    jenisPermohonan: filter.jenisPermohonan,
-                  },
-                })
-              }
+            <Text
+              style={{
+                fontSize: 20,
+                color: colors.darkBlue,
+                fontFamily: "FiraSansMedium",
+                marginBottom: 4,
+              }}
             >
-              Pilih Kuota
-            </Button>
+              {item.lokasi.nama}
+            </Text>
+            <Text
+              style={{
+                fontSize: 14,
+                color: colors.inactive,
+                fontFamily: "FiraSansRegular",
+                marginBottom: 8,
+              }}
+            >
+              {item.lokasi.alamat}
+            </Text>
+            <View
+              style={{ flexDirection: "row", alignItems: "center", gap: 10 }}
+            >
+              <Text
+                style={{
+                  fontSize: 15,
+                  color: "black",
+                  fontFamily: "FiraSansRegular",
+                }}
+              >
+                Kuota permohonan reguler :
+              </Text>
+              <View
+                style={{
+                  backgroundColor: colors.darkGreen,
+                  borderRadius: "15",
+                  // justifyContent: "center",
+                  alignItems: "center",
+                  alignContent: "center",
+                  width: 30, // Equal width and height
+                  height: 30,
+                }}
+              >
+                <Text
+                  style={{
+                    fontSize: 18,
+                    color: "white",
+                    fontFamily: "FiraSansRegular",
+                    textAlign: "center",
+                    margin: "auto",
+                  }}
+                >
+                  {item.totalKuota}
+                </Text>
+              </View>
+            </View>
+            <View style={{ marginBottom: 10, flexDirection: "row", gap: 10 }}>
+              <View
+                style={{ flexDirection: "row", gap: 6, alignItems: "center" }}
+              >
+                <Ionicons
+                  name={
+                    item.lokasi.isBiasa ? "checkmark-circle" : "close-circle"
+                  }
+                  size={24}
+                  color={item.lokasi.isBiasa ? "green" : "red"}
+                />
+                <Text style={{ fontFamily: "FiraSansRegular", color: "black" }}>
+                  Biasa
+                </Text>
+              </View>
+              <View
+                style={{ flexDirection: "row", gap: 6, alignItems: "center" }}
+              >
+                <Ionicons
+                  name={
+                    item.lokasi.isElektronik
+                      ? "checkmark-circle"
+                      : "close-circle"
+                  }
+                  size={24}
+                  color={item.lokasi.isElektronik ? "green" : "red"}
+                />
+                <Text style={{ fontFamily: "FiraSansRegular", color: "black" }}>
+                  Elektronik
+                </Text>
+              </View>
+              <View
+                style={{ flexDirection: "row", gap: 6, alignItems: "center" }}
+              >
+                <Ionicons
+                  name={
+                    item.lokasi.isPolikarbonat
+                      ? "checkmark-circle"
+                      : "close-circle"
+                  }
+                  size={24}
+                  color={item.lokasi.isPolikarbonat ? "green" : "red"}
+                />
+                <Text style={{ fontFamily: "FiraSansRegular", color: "black" }}>
+                  Polikarbonat
+                </Text>
+              </View>
+            </View>
+            <View
+              style={{ flexDirection: "row", justifyContent: "space-between" }}
+            >
+              <Button
+                onPress={() =>
+                  router.push({
+                    pathname: "/lokasi/pilihJadwal",
+                    params: {
+                      lokasi: JSON.stringify(item),
+                      jenisPermohonan: filter.jenisPermohonan,
+                    },
+                  })
+                }
+                style={{
+                  width: "48%",
+                  backgroundColor: colors.darkBlue,
+                  height: 48,
+                  borderRadius: 12,
+                }}
+              >
+                <Text
+                  style={{
+                    color: "white",
+                    fontSize: 16,
+                    fontFamily: "FiraSansMedium",
+                  }}
+                >
+                  Pilih Kuota
+                </Text>
+              </Button>
 
-            <Button
-              onPress={() =>
-                router.push({
-                  pathname: "/lokasi/detailLokasi",
-                  params: {
-                    lokasi: JSON.stringify(item),
-                  },
-                })
-              }
-            >
-              Lihat Detail
-            </Button>
-            {/* <Button title="Choose" onPress={() => navigation.push('CalendarPage', { location: item.location })} />
-            <Button title="See Detail" onPress={() => navigation.push('DetailPage', { location: item.location })} /> */}
+              <Button
+                onPress={() =>
+                  router.push({
+                    pathname: "/lokasi/detailLokasi",
+                    params: {
+                      lokasi: JSON.stringify(item),
+                    },
+                  })
+                }
+                style={{
+                  width: "48%",
+                  backgroundColor: colors.white,
+                  height: 48,
+                  borderRadius: 12,
+                  borderWidth: 1.5,
+                }}
+              >
+                <Text
+                  style={{
+                    color: colors.darkBlue,
+                    fontSize: 16,
+                    fontFamily: "FiraSansMedium",
+                  }}
+                >
+                  Lihat Detail
+                </Text>
+              </Button>
+            </View>
           </View>
         ))}
         <Text>Results without Quota</Text>
